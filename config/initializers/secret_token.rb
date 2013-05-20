e4b238d0a9b750a6349588a92abab83fd755e5c9
jaynejb <jayne.j.belz@gmail.com>
2013-05-20 18:17:49 +0900

    bloc blog app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlocBlog::Application.config.secret_token = '0572472c54c3a0902669a588a77b1d9d7ab7c20608f218fb682b560c7a38304efbe266d70d9ddf30642ba6eea470252dedc6acefc4aa756aebab55346ef5d4e4'
